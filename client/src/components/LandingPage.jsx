a074edea398e51c90b662f169c72c1538de25bca
ian_git8520 <iandavidnjenga@gmail.com>
2026-01-28 00:42:39 +0300

    creating landing page content

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -6,3 +6,10 @@ const Landing = () => {
   return (
     <div className="landing-container">
       <div className="animated-bg"></div>
+
+
+      <div className="landing-content">
+        <h1 className="animated-text">Welcome to SendIT 🚀</h1>
+        <p className="animated-text delay1">
+          Fast, reliable, and simple courier service at your fingertips.
+        </p>
